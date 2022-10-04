da411794d827d0fb8f37bcb049aca558b85e49f2
Kelvin Costa Da Silveira <kelvin.silveira@andrademaia.com>
2022-10-04 15:19:18 -0300

    atualização_1.0

--- alpha_1.0.py
+++ alpha_1.0.py
@@ -1,12 +1,15 @@
+"Bibliotecas"
 import fileinput
 import sys
 import time
 import subprocess
-sys.platform
-'win64'
-"Executar o programa Shodo 1.1.2"
 import os
+from tkinter import *
+"Janela para esconder o terminal"
+'win64'
+sys.platform
+"Executar o programa Shodo 1.1.2"
 os.startfile("C:\Program Files (x86)\Shodo\shodo.exe")
-time.sleep(30)
+time.sleep(60)
 subprocess.call("TASKKILL /F /IM shodo.exe", shell=True)
 subprocess.call("TASKKILL /F /IM javaw.exe", shell=True)
